765ffd5e1b08dc4a9518c0ca380f415a20fe4169
Benjamin Maer <ben@resplendent.co>
2016-01-25 16:31:28 -0500

    Set up cocoapod project.

---
+++ Example/Pods/Target Support Files/Pods-RTSMTableSectionManager_Example/Pods-RTSMTableSectionManager_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_RTSMTableSectionManager_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_RTSMTableSectionManager_ExampleVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-RTSMTableSectionManager_Tests/Pods-RTSMTableSectionManager_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RTSMTableSectionManager_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_RTSMTableSectionManager_Tests
+@end
