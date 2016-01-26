f24d0309ebe5e81220cd645ad07fca64c9fec79b
Benjamin Maer <ben@resplendent.co>
2016-01-26 15:57:45 -0500

    Updated Example Project

--- Example/Pods/Target Support Files/RTSMTableSectionManager/RTSMTableSectionManager-umbrella.h
+++ Example/Pods/Target Support Files/RTSMTableSectionManager/RTSMTableSectionManager-umbrella.h
@@ -3,6 +3,7 @@
 #import "RTSMTableSectionManager.h"
 #import "RTSMTableSectionManagerProtocols.h"
 #import "RTSMTableSectionRangeManager.h"
+#import "RTSMTableSectionRangeManager_SectionLengthDelegate.h"
 
 FOUNDATION_EXPORT double RTSMTableSectionManagerVersionNumber;
 FOUNDATION_EXPORT const unsigned char RTSMTableSectionManagerVersionString[];
